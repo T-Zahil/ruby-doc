316ef1305365c11f81c258053af2dfd0a674fe56
Thomas Sanlis <Thomas.sanlis@gmail.com>
2017-01-30 09:53:58 +0100

    [chapter]
    Add others date strategy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
   def sexy_date(date)
+    # Methode 1 : TRY
     return date.try(:strftime, "%d/%m/%Y")
+
+    # Methode 2 : TERNAIRE
+    # return date.nil? ? "Date inconnue" : date.strftime("%d/%m/%Y")
+    
+    # Methode 3 : RESCUE
+    # return date.strftime("%d/%m/%Y") rescue "Date inconnue"
   end
 end
