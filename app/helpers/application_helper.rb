dc165b5c300b7cd3a4f999a457addab4a516c370
Thomas Sanlis <Thomas.sanlis@gmail.com>
2017-01-30 10:33:38 +0100

    [CHAPTERS] Add rescue on article published date

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
   def sexy_date(date)
     # Methode 1 : TRY
-    return date.try(:strftime, "%d/%m/%Y")
+    # return date.try(:strftime, "%d/%m/%Y")
 
     # Methode 2 : TERNAIRE
     # return date.nil? ? "Date inconnue" : date.strftime("%d/%m/%Y")
     
     # Methode 3 : RESCUE
-    # return date.strftime("%d/%m/%Y") rescue "Date inconnue"
+    return date.strftime("%d/%m/%Y") rescue "Date inconnue"
   end
 
   def safe_time_ago_in_words(date)
     if (date.nil?)
-      return ""
+      return "Date inconnue"
     else
       return time_ago_in_words(date)
     end
